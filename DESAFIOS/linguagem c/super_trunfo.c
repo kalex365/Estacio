ef85c12383c2698195d892d5037a0e8bd8f9cacf
Alexandre Maria da Conceição <142837105+kalex365@users.noreply.github.com>
2025-12-23 22:39:01 +0000

    teste

--- DESAFIOS/linguagem c/super_trunfo.c
+++ DESAFIOS/linguagem c/super_trunfo.c
@@ -3,20 +3,29 @@
 int main (){
 
     int carta, populacao;
-    char Estado[3], cidade[15];
+    char Estado;
+    char codigo[3];
+    char cidade[15];
     float area, pib, ponto;
 
-
     carta = 1;
-    Estado = A;
+    Estado = "A";
     carta = 1;
-    codigo = A01;
-    cidade = São Paulo;
+    codigo = "A01";
+    cidade = "São Paulo";
     populacao = 12325000;
     area = 1521.11;
     pib = 699.28;
     ponto = 50;
 
+    printf("Carta 1: \n");
+    printf("Estado: \n");
+    printf("Código: \n");
+    printf("Nome da Cidade: \n");
+    printf("População: \n");
+    printf("Área: \n");
+    printf("PIB: \n");
+    printf("Número de Pontos Turísticos: \n");
 
 
 
